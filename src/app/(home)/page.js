5a9921d9585771e0b4e285e8ee3add1a83a00aaf
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2024-08-16 02:01:02 +0600

    updated

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -5,36 +5,35 @@ import Running_Banner from "@/components/Pages/Marquee";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
-
-export default function Home() {
+ const Home=()=> {
 
   const [data, setData] = useState([]);
-  const url = "http://localhost:3000";
-  
- useEffect(() => {
-  const FetchData = async () => {
-    try {
-      const response = await axios.get(`${url}/api`);
-      setData(response.data);
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-    }
-  }
-  FetchData();
- }
 
-  , []);
+  useEffect(() => {
+    const FetchData = async () => {
+      try {
+        const response = await axios.get(`https://kiskfm.vercel.app/api`);
+        setData(response.data);
+        console.log(response)
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
 
-  console.log(data)
+      FetchData();
+
+  }, []);
+
+  console.log(data);
   return (
     <main className=" px-4 md:px-6 lg:px-20">
       <Running_Banner />
       {/* Main content */}
       <div className="grid grid-cols-6">
-        <div className="col-span-5"> {data?.length}
-        </div>
+        <div className="col-span-5"> {data?.length}</div>
       </div>
     </main>
   );
 }
+
+export default Home;
